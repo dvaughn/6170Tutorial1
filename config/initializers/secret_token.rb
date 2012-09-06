d2a6595dc62130da94f30542d0acc377d4b8deec
saasbook <saasbook@saasbook.(none)>
2012-09-06 11:53:11 -0700

    hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '542c0baa9106b847dab61e1f523568068898126390971ad7408de93acf46f3917219b237df7daec6bfa2ffa03990c95c350fcd0f6d91068cab53f951fb107584'
